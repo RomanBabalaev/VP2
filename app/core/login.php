eed575ced69601ed2dbe3e8fee57bb4ad3c69ca2
Roman <87jormen@gmail.com>
2018-04-14 23:57:38 -0400

    VP2

---
+++ app/core/login.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "db" => [
+        'host' => 'localhost',
+        'dbname' => 'vp2',
+        'user' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ],
+];
